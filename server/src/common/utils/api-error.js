6fb10cf5d501b3bc61c19f7ae364eafd1f53c19c
Siddhant <siddhantvgorte@gmail.com>
2026-08-04 18:46:55 +0530

    feat: add common API utilities

---
+++ server/src/common/utils/api-error.js
@@ -0,0 +1,29 @@
+class ApiError extends Error {
+    constructor (statuscode, message) {
+        super(message)
+        this.statuscode = statuscode
+        this.isOperational = true
+        Error.captureStackTrace(this, this.constructor)
+    }
+
+    static badRequest(message = "Bad Request") {
+        return new ApiError(400, message)
+    }
+    static unauthorized(message = "Unauthorized") {
+        return new ApiError(401, message)
+    }
+    
+    static conflict(message = "Conflict") {
+        return new ApiError(409, message)
+    }
+    
+    static forbidden(message = "Forbidden") {
+        return new ApiError(403, message)
+    }
+    
+    static notFound(message = "Not Found") {
+        return new ApiError(404, message)
+    }
+}
+
+export default ApiError
